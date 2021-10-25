b1db484b5b35b633229973832afc7412c88a96af
JotaBord <jorgebordignon@gmail.com>
2021-10-25 10:24:25 -0300

    Layout responsivo + forms

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -1,15 +1,26 @@
 import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.scss']
+  styleUrls: ['./titulo.component.scss'],
 })
 export class TituloComponent implements OnInit {
   @Input() titulo!: string;
-  constructor() { }
+  @Input() subtitulo = 'Desde 2021';
+  @Input() iconclass = 'fa fa-user';
+  @Input() botaoListar = false;
 
-  ngOnInit(): void {
+  // public eventos: Evento[] = [];
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void { }
+
+  listar(): void {
+    // console.log("teste", this.titulo);
+    this.router.navigate([`/${this.titulo.toLocaleLowerCase()}/lista`]);
   }
 }
